90ccfb152200cf9bb525264c584071e4a142ef3d
Maanil Verma <vermaMaanil97@gmail.com>
2024-04-24 16:22:10 +0530

    chore: update prisma schema and migrations

---
+++ prisma/migrations/20240424091602_addindexes/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "UserSavedInterests_userId_idx" ON "UserSavedInterests"("userId");
+
+-- CreateIndex
+CREATE INDEX "UserSavedInterests_categoryId_idx" ON "UserSavedInterests"("categoryId");
